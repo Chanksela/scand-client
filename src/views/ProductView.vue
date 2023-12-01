b5b07c47bcc7e0d014db0369ef2aa0f8cead655d
Kakha <kakhachankseliani@gmail.com>
2023-12-01 15:45:55 +0400

    chore: added placeholders and description

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -5,15 +5,33 @@
 			<section class="product-main-input">
 				<div class="input-wrapper">
 					<label for="sku">SKU</label>
-					<input type="text" id="sku" name="sku" required />
+					<input
+						type="text"
+						id="sku"
+						name="sku"
+						placeholder="Please enter the SKU"
+						required
+					/>
 				</div>
 				<div class="input-wrapper">
 					<label for="name">Name</label>
-					<input type="text" id="name" name="name" required />
+					<input
+						type="text"
+						id="name"
+						name="name"
+						placeholder="Please enter the name of the product"
+						required
+					/>
 				</div>
 				<div class="input-wrapper">
-					<label for="price">Price</label>
-					<input type="number" id="price" name="price" required />
+					<label for="price">Price ($)</label>
+					<input
+						type="number"
+						id="price"
+						name="price"
+						placeholder="Please enter the price of the Product"
+						required
+					/>
 				</div>
 			</section>
 			<section class="product-switcher">
@@ -31,9 +49,16 @@
 				id="DVD"
 				v-if="productType === 'DVD'"
 			>
+				<p>Please enter the size of DVD in MB</p>
 				<div class="input-wrapper">
 					<label for="size">Size (MB)</label>
-					<input type="number" id="size" name="size" required />
+					<input
+						type="number"
+						id="size"
+						name="size"
+						placeholder="Size of dvd in MB"
+						required
+					/>
 				</div>
 			</section>
 			<section
@@ -41,17 +66,36 @@
 				id="Furniture"
 				v-else-if="productType === 'Furniture'"
 			>
+				<p>Please enter dimensions of Furniture in HxWxL</p>
 				<div class="input-wrapper">
 					<label for="height">Height (CM)</label>
-					<input type="number" id="height" name="height" required />
+					<input
+						type="number"
+						id="height"
+						name="height"
+						placeholder="Height in CM"
+						required
+					/>
 				</div>
 				<div class="input-wrapper">
 					<label for="width">Width (CM)</label>
-					<input type="number" id="width" name="width" required />
+					<input
+						type="number"
+						id="width"
+						name="width"
+						placeholder="Width in CM"
+						required
+					/>
 				</div>
 				<div class="input-wrapper">
 					<label for="length">Length (CM)</label>
-					<input type="number" id="length" name="length" required />
+					<input
+						type="number"
+						id="length"
+						name="length"
+						placeholder="Length in CM"
+						required
+					/>
 				</div>
 			</section>
 			<section
@@ -59,9 +103,16 @@
 				id="Book"
 				v-else-if="productType === 'Book'"
 			>
+				<p>Please enter the book in KG</p>
 				<div class="input-wrapper">
 					<label for="weight">Weight (KG)</label>
-					<input type="number" id="weight" name="weight" required />
+					<input
+						type="number"
+						id="weight"
+						name="weight"
+						placeholder="Weight in KG"
+						required
+					/>
 				</div>
 			</section>
 		</form>
